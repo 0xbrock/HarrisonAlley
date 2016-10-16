2761ce420d4e22d36b1e48c0b510c0724f21c979
David Bohan <madvlad00@googlemail.com>
2016-10-16 17:32:26 -0400

    Add projectiles

---
+++ Assets/Scripts/Shooter.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Shooter : MonoBehaviour {
+
+	public GameObject projectile;
+	public AudioClip shootSound;
+
+	private AudioSource source;
+	private Camera camera;
+
+	// Use this for initialization
+	void Start () {
+		source = GetComponent<AudioSource> ();
+		camera = GetComponent<Camera> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		foreach (var touch in Input.touches) {
+			if (touch.phase == TouchPhase.Began) {
+				source.PlayOneShot (shootSound);
+				shootRay ();
+			}
+		}
+	}
+
+	private void shootRay() {
+		var bullet = Instantiate(projectile);
+		bullet.transform.position = gameObject.transform.position + gameObject.transform.forward;
+		var rb = bullet.GetComponent<Rigidbody> ();
+		rb.AddForce (transform.forward * 1500);
+	}
+}
